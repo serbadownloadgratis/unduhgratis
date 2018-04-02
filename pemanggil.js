48b5109cb3ced91ac1e646d9a12d19f50d644d71
serbadownloadgratis <37610958+serbadownloadgratis@users.noreply.github.com>
2018-04-02 14:20:33 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -26,7 +26,7 @@ if(x=="2"){xxx="/2018/03/cara-mengatasi-error-404.html"}
 if(x=="3"){xxx="/2018/03/cara-membuat-sitemap-sendiri-di-blog.html"}
 if(x=="4"){xxx="/2018/03/yamaha-r25.html"}
 if(x=="5"){xxx="/2018/03/suzuki-gsx-r150.html"}
-a_to_ve[i].href="http://www.tunggu.ga/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+a_to_ve[i].href="http://tunggu.ga/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
