7293d5c885793f0d88bc4f2522e9cbdb67ab642a
serbadownloadgratis <37610958+serbadownloadgratis@users.noreply.github.com>
2018-04-03 10:21:37 +0700

    Update pemanggil.js

--- pemanggil.js
+++ pemanggil.js
@@ -21,11 +21,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(x=="1"){xxx="/2018/03/cara-membuat-email-gmail.html"}
-if(x=="2"){xxx="/2018/03/cara-mengatasi-error-404.html"}
-if(x=="3"){xxx="/2018/03/cara-membuat-sitemap-sendiri-di-blog.html"}
-if(x=="4"){xxx="/2018/03/yamaha-r25.html"}
-if(x=="5"){xxx="/2018/03/suzuki-gsx-r150.html"}
+if(x=="1"){xxx="/2018/03/pengertian-dari-gmail_22.html"}
+if(x=="2"){xxx="/2018/03/arti-error-404_22.html"}
+if(x=="3"){xxx="/2018/03/cara-membuat-sitemap-sendiri-di-blog_97.html"}
+if(x=="4"){xxx="/2018/03/yamaha-r25_23.html"}
+if(x=="5"){xxx="/2018/03/suzuki-gsx-r150_23.html"}
 a_to_ve[i].href="https://bentarduluya.blogspot.com/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
